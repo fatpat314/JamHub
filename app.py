c55fe2b83c972311321a02efbf14c774b67792ee
Patrick <w.patrick.kelly@gmail.com>
2019-11-20 11:12:20 -0800

    nav bar and pages

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    "Show homepage"
+    return render_template('index.html', index=index)
+
+@app.route('/sign-in')
+def sign_in():
+    "sign in page"
+    return render_template('sign_in.html', sigh_in=sign_in)
+
+@app.route('/sign-up')
+def sign_up():
+    "sign_up page"
+    return render_template('sign_up.html', sigh_up=sign_up)
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', profile=profile)
+
+if __name__ == '__main__':
+    app.run(debug=True)
